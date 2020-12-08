9b73581d962e54d31c08d3e341c2ac945889dd77
Wei-Chen Lin <70911832+lin-weichen@users.noreply.github.com>
2020-12-08 14:24:15 +0800

    Update db-config.js

--- jobs_server/db-config.js
+++ jobs_server/db-config.js
@@ -1,8 +1,8 @@
 // Private. Will not be included in submission
 module.exports = {
-  host: "127.0.0.1",
-  port: "3306",
-  user: "student",
-  password: "",
-  database: "<database name>"
+  host: "cis550project.cwbzjqq78xlo.us-east-1.rds.amazonaws.com",
+  port: "1521",
+  user: "admin",
+  password: "weichenlin",
+  database: "JobDB"
 };
